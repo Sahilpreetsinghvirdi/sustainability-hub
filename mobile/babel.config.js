b466cf418cee905d9cbb6925a04c9a7d941b3cc0
Sahil Virdi <ksahilpreetsingh1980@gmail.com>
2026-08-19 15:50:58 +0530

    fix: add react-native-worklets for reanimated v4

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -20,6 +20,7 @@ module.exports = function(api) {
           '@/constants': './src/constants',
         },
       }],
+      'react-native-worklets/plugin',
       'react-native-reanimated/plugin',
     ],
   };
